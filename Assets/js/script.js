1392d500c759fd58ebd8935068ec9f22c96f1aa1
Joaquin Salazar <joaqsala@gmail.com>
2023-03-12 19:56:32 -0500

    added dayjs to show in header

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,14 @@
+// $(document).ready(function(){
+  
+// })
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+
 $(function () {
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +27,11 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  
+  let today = dayjs().format("dddd, MMMM D")
+  $("#currentDay").text(today);
 });
+
+// window.onload = function(){
+//   //whatever first function is being used
+// }
