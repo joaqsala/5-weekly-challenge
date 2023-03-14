24f0559ff094132f43380e1eb50c384d0ad2a4f7
Joaquin Salazar <joaqsala@gmail.com>
2023-03-13 22:56:10 -0500

    added content to README file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,69 +2,70 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-$(document).ready(function(){
-
+$(document).ready(function () {
   //displays the current date in the header of the page
-  var today = dayjs().format("dddd, MMMM D")
+  var today = dayjs().format("dddd, MMMM D");
   $("#currentDay").text(today);
 
   //gets dayjs info and sets currentTime to 24-hr format
-  var currentTime = dayjs().format('H');
-  console.log(currentTime)
+  var currentTime = dayjs().format("H");
+  // console.log(currentTime);
 
   //sets the display of the hours column for the timeframe of 8am to 6pm
   for (var i = 8; i <= 18; i++) {
     var hourText;
     if (i < 12) {
-      hourText = `${i}AM`
-    } else if
-      (i == 12) {
-      hourText = "12PM"
+      hourText = `${i}AM`;
+    } else if (i == 12) {
+      hourText = "12PM";
     } else {
-      hourText = `${i - 12}PM`
+      hourText = `${i - 12}PM`;
     }
-    console.log(hourText)
+    // console.log(hourText);
 
-  //gets the saved localStorage using the row id
-  var planner = localStorage.getItem(i)
-  
-  //creates rows and columns with classes and attributees
-  var row = $("<div>").attr("id", `${i}`).addClass("row time-block");
-  var hourCol = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(`${hourText}`);
-  row.append(hourCol);
+    //gets the saved localStorage using the row id
+    var planner = localStorage.getItem(i);
 
-  //creates text area and appends classes/attributes; sets value to planner value from localStorage
-  var description = $("<textarea>").addClass("col-8 col-md-10 description")
-  description.text(planner)
-  row.append(description)
+    //creates rows and columns with classes and attributees
+    var row = $("<div>").attr("id", `${i}`).addClass("row time-block");
+    var hourCol = $("<div>")
+      .addClass("col-2 col-md-1 hour text-center py-3")
+      .text(`${hourText}`);
+    row.append(hourCol);
 
-  //creates save and appends button and appends classes/attributes
-  var saveButton = $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save").append($("<i>").addClass("fas fa-save").attr("aria-hidden", true));
-  row.append(saveButton)
+    //creates text area and appends classes/attributes; sets value to planner value from localStorage
+    var description = $("<textarea>").addClass("col-8 col-md-10 description");
+    description.text(planner);
+    row.append(description);
 
-  
-  // compares the currentTime to and adds classes correspondingly
-  if (i < currentTime){
-    row.addClass("past")
-  } else if (i == currentTime) {
-    row.addClass("present")
-  } else {
-    row.addClass("future")
+    //creates save and appends button and appends classes/attributes
+    var saveButton = $("<button>")
+      .addClass("btn saveBtn col-2 col-md-1")
+      .attr("aria-label", "save")
+      .append($("<i>").addClass("fas fa-save").attr("aria-hidden", true));
+    row.append(saveButton);
+
+    // compares the currentTime to and adds classes correspondingly
+    if (i < currentTime) {
+      row.addClass("past");
+    } else if (i == currentTime) {
+      row.addClass("present");
+    } else {
+      row.addClass("future");
+    }
+
+    //appends the rows to the container
+    $(".container-lg.px-5").append(row);
+
+    // adds a click listener on the save button
+    $(".saveBtn").click(function () {
+      //saves the id in the parent(containing time-block)as the key to be used in localStorage
+      var id = $(this).parent().attr("id");
+      //saves the value of the user generated text to be used in localStorage
+      var planner = $(this).siblings("textarea").val();
+
+      //saves the key and text area contentd(value) to local storage
+      localStorage.setItem(id, planner);
+    });
   }
-
-  //appends the rows to the container
-  $(".container-lg.px-5").append(row);
-
-
-  // adds a click listener on the save button
-  $(".saveBtn").click(function () {
-    //saves the id in the parent(containing time-block)as the key to be used in localStorage
-    var id = $(this).parent().attr("id")
-    //saves the value of the user generated text to be used in localStorage
-    var planner = $(this).siblings("textarea").val()
-
-    //saves the key and text area contentd(value) to local storage 
-    localStorage.setItem(id, planner)
-  })
-  }
-})
+});
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # 5-weekly-challenge
-Work Day Scheduler
+Week 5 Challenge - Work Day Scheduler
+
+## Description
+
+This repository contains the files for a work day scheduler. Using JavaScript, Jquery, Bootstrap, and dayJS, a day planner is created and displayed. The rows for the hours allow the user to input their tasks and save them to localStorage. The color of the rows change dynamically based on whether the time/task is in the past, present, or future. 
+
+## Usage
+
+![Work day scheduler with some inputs](Assets/images/screenshot-challenge-5.png)
+https://joaqsala.github.io/5-weekly-challenge/
+
+This webpage contains a workday planner with todays date displayed at the top. The user can add and save tasks to the planner to save for later or to see on another device. With past events shown in grey, the present shown in red, and the future shown in green, the user will have an easy time keeping up the day's to-do list.
+
+
+## Credits
+
+To my University of Texas Bootcamp Cohort, the course TA's and instructor, and other tutors who helped guide me. 
+
+
+## License
+
+Please refer to LICENSE in repo.
